30474671f77e65d9886c0eef98b1c3e1492486db
Ayun-xin <2582357590@qq.com>
2025-06-16 15:29:18 +0800

    Update listdemo.py

--- listdemo.py
+++ listdemo.py
@@ -21,7 +21,7 @@ from openai import OpenAI
 
 client = OpenAI(
     # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：api_key="sk-xxx",
-    api_key=os.getenv(API_key),  # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
+    api_key=API_key,  # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
